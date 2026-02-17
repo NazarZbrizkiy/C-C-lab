d867c79f0887aa8e5a121eef54e0a2c9ab581cda
NazarZbrizkiy <nazarzbrizkiy@knu.ua>
2026-02-17 14:42:16 +0200

    ClassWork

---
+++ 1_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int x1 = 2 + 31;
+    printf("x1 = %d\n", x1);
+    int x2 = 45 * 54 - 11;
+    printf("x2 = %d\n", x2);
+    double x3 = 15.0 /4;
+    printf("x3 = %lf\n", x3);
+    double x4 = 67%5;
+    printf("x4 = %lf\n", x4);
+    double x5 = (2 * 45.1  + 3.2) / 2;
+    printf("x5 = %lf\n", x5);
+    
+        return 0;
+}
