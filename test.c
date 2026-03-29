f56440a7d58597da109ddc019d5e4ff82344df26
space <space@MacBook-Air--Nazar.local>
2026-03-29 20:43:18 +0300

    Classwork9_1__9_4

--- test.c
+++ test.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main() {
-    printf("Hello, World!\n");
+    printf("Enter a number: ");
+    int num;
+    scanf("%d", &num);
+    printf("You entered: %d\n", num);
     return 0;
 }
